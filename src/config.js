6af23c0d099eec623aa37a413b7eb7e1eda691ff
JongilSohn <sji7532@gmail.com>
2020-10-29 22:42:42 +0900

    nike clone 프로젝트 구현 완료

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const signUpAPI = "http:/192.168.0.7:8000/users/signup";
+export const signInAPI = "http://192.168.0.7:8000/users/signin";
+export const productAPI = "http://192.168.0.7:8000/product";
